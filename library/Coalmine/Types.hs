e85a6d5b9c20501572a0eb696c5746ae6f6af206
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-12-17 03:18:48 +0300

    Add vector aliases

--- library/Coalmine/Types.hs
+++ library/Coalmine/Types.hs
@@ -5,6 +5,12 @@ module Coalmine.Types where
 
 import Prelude
 import qualified Text.Builder as TextBuilder
+import qualified Data.Vector.Unboxed
+import qualified Data.Vector
 
 
 type TextBuilder = TextBuilder.Builder
+
+type UVec = Data.Vector.Unboxed.Vector
+
+type Vec = Data.Vector.Vector
