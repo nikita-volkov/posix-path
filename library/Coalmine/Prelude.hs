e4efc17a746a7c46d139bc9a0fee08303ddf4643
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-12-18 16:10:52 +0300

    Sort prelude

--- library/Coalmine/Prelude.hs
+++ library/Coalmine/Prelude.hs
@@ -6,20 +6,20 @@ module Coalmine.Prelude
 )
 where
 
-import Prelude hiding (Vector)
 import Coalmine.Applicative as Exports
 import Coalmine.Building as Exports
 import Coalmine.Lens as Exports
 import Coalmine.Parsing as Exports
 import Coalmine.Types as Exports
-import Reduction as Exports (Reduction)
-import Data.DoubleWord as Exports (Word96(..), Word128(..), Word160(..), Word192(..), Word224(..), Word256(..))
-import Deque.Strict as Exports (Deque)
 import Control.Foldl as Exports (Fold(..), FoldM(..))
-import Data.Tuple.All as Exports
+import Data.DoubleWord as Exports (Word96(..), Word128(..), Word160(..), Word192(..), Word224(..), Word256(..))
 import Data.String.ToString as Exports
+import Data.Tuple.All as Exports
 import Data.Vector.Generic as Exports (Vector)
+import Deque.Strict as Exports (Deque)
 import Network.IP.Addr as Exports (IP(..), IP4(..), IP6(..), NetAddr(..), InetAddr(..), InetPort(..))
+import Prelude hiding (Vector)
+import Reduction as Exports (Reduction)
 
 
 showAsText :: Show a => a -> Text
