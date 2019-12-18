e262f884909a126398ec796a7d9d5f5d5eb7a0f6
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-12-18 15:47:44 +0300

    Add more network types

--- library/Coalmine/Prelude.hs
+++ library/Coalmine/Prelude.hs
@@ -19,7 +19,7 @@ import Control.Foldl as Exports (Fold(..), FoldM(..))
 import Data.Tuple.All as Exports
 import Data.String.ToString as Exports
 import Data.Vector.Generic as Exports (Vector)
-import Network.IP.Addr as Exports (IP(..), IP4(..), IP6(..), NetAddr(..))
+import Network.IP.Addr as Exports (IP(..), IP4(..), IP6(..), NetAddr(..), InetAddr(..), InetPort(..))
 
 
 showAsText :: Show a => a -> Text
