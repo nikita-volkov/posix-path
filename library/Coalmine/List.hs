f58c3731a6efb2669317adbb4257d4b9e210a816
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-03-16 21:07:15 +0300

    Add nubSort

---
+++ library/Coalmine/List.hs
@@ -0,0 +1,11 @@
+module Coalmine.List where
+
+import Prelude
+import qualified Data.Map as Map
+
+
+nubSort :: Ord a => [a] -> [a]
+nubSort = nubSortOn id
+
+nubSortOn :: Ord b => (a -> b) -> [a] -> [a]
+nubSortOn mapper = Map.elems . foldr (\ a -> Map.insert (mapper a) a) Map.empty
