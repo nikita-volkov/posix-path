d7b5907a0bc403b3ac7b6bc59bcf390cf6140627
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-03-22 03:01:40 +0300

    Add S3

--- library/Coalmine/Fx.hs
+++ library/Coalmine/Fx.hs
@@ -2,7 +2,20 @@ module Coalmine.Fx where
 
 import Prelude
 import Fx
+import qualified Turtle
 
 
 runFxFailing :: Show err => Fx () err a -> IO a
 runFxFailing = runFx . handleErr (fail . show)
+
+runCmd :: Text -> Fx env Int ()
+runCmd cmd = do
+  exitCode <- runTotalIO (Turtle.shell cmd empty)
+  case exitCode of
+    Turtle.ExitSuccess -> return ()
+    Turtle.ExitFailure code -> throwErr code
+
+compressFile :: FilePath -> Fx env Int FilePath
+compressFile path = do
+  runCmd ("xz -zfq7e " <> fromString path)
+  return (path <> ".xz")
---
+++ library/Coalmine/S3.hs
@@ -0,0 +1,48 @@
+module Coalmine.S3 where
+
+import Prelude
+import NeatInterpolation
+import Fx
+import qualified Coalmine.Fx as Fx
+
+
+data StorageClass =
+  StandardStorageClass |
+  ReducedRedundancyStorageClass |
+  StandardIaStorageClass |
+  OneZoneIaStorageClass |
+  IntelligentTieringStorageClass |
+  GlacierStorageClass |
+  DeepArchiveStorageClass
+
+data Acl =
+  PrivateAcl |
+  PublicReadAcl |
+  PublicReadWriteAcl |
+  AuthenticatedReadAcl |
+  AwsExecReadAcl |
+  BucketOwnerReadAcl |
+  BucketOwnerFullControlAcl |
+  LogDeliveryWriteAcl
+
+uploadToS3 :: StorageClass -> Acl -> Text -> FilePath -> Fx env Int ()
+uploadToS3 storageClass acl s3Dir file = Fx.runCmd cmdText where
+  cmdText = [text|aws s3 cp --quiet --storage-class=$storageClassText --acl=$aclText $sourceText $s3Dir|]
+  storageClassText = case storageClass of
+    StandardStorageClass -> "STANDARD"
+    ReducedRedundancyStorageClass -> "REDUCED_REDUNDANCY"
+    StandardIaStorageClass -> "STANDARD_IA"
+    OneZoneIaStorageClass -> "ONE-ZONE_IA"
+    IntelligentTieringStorageClass -> "INTELLIGENT_TIERING"
+    GlacierStorageClass -> "GLACIER"
+    DeepArchiveStorageClass -> "DEEP_ARCHIVE"
+  sourceText = fromString file
+  aclText = case acl of
+    PrivateAcl -> "private"
+    PublicReadAcl -> "public-read"
+    PublicReadWriteAcl -> "public-read-write"
+    AuthenticatedReadAcl -> "authenticated-read"
+    AwsExecReadAcl -> "aws-exec-read"
+    BucketOwnerReadAcl -> "bucket-owner-read"
+    BucketOwnerFullControlAcl -> "bucket-owner-full-control"
+    LogDeliveryWriteAcl -> "log-delivery-write"
