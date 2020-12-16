f5624f1e5475eb716eaa071e37d95fd685103b3a
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-12-16 22:10:11 +0300

    Add hashableUnique

--- library/Coalmine/Optics.hs
+++ library/Coalmine/Optics.hs
@@ -2,6 +2,7 @@ module Coalmine.Optics where
 
 import Coalmine.InternalPrelude
 import qualified MooreMachines as Mm
+import qualified Data.HashSet as HashSet
 
 
 feedingMooreOf :: Fold a b -> Moore b c -> a -> Moore b c
@@ -10,3 +11,7 @@ feedingMooreOf optic =
   where
     step moore b =
       Mm.feeding b moore
+
+hashableUnique :: (Eq a, Hashable a) => Fold a a
+hashableUnique =
+  folding HashSet.singleton
