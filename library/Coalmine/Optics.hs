43f19e42ea37b341414de4108f6dc3e4eebdb96c
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-12-17 11:58:09 +0300

    Add map assocs

--- library/Coalmine/Optics.hs
+++ library/Coalmine/Optics.hs
@@ -2,6 +2,8 @@ module Coalmine.Optics where
 
 import Coalmine.InternalPrelude
 import qualified Coalmine.HashSet as HashSet
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Map.Strict as Map
 import qualified MooreMachines as Mm
 
 
@@ -21,3 +23,23 @@ feedingMooreOf optic =
 uniqueFolded :: (Eq a, Hashable a, Foldable f) => Fold (f a) a
 uniqueFolded =
   folding HashSet.fromFoldable
+
+{-|
+
+>>> toListOf mapAssoc (Map.fromList [('b',2),('c',3),('a',1)])
+[('a',1),('b',2),('c',3)]
+
+-}
+mapAssoc :: Fold (Map k v) (k, v)
+mapAssoc =
+  folding Map.toList
+
+{-|
+
+>>> toListOf hashMapAssoc (HashMap.fromList [('b',2),('c',3),('a',1)])
+[('a',1),('b',2),('c',3)]
+
+-}
+hashMapAssoc :: Fold (HashMap k v) (k, v)
+hashMapAssoc =
+  folding HashMap.toList
