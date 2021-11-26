cacb915325b02b61762ebf3dc68b27f6b6349f0a
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-11-26 18:51:20 +0300

    Sketch

--- library/Coalmine/Streamer.hs
+++ library/Coalmine/Streamer.hs
@@ -1,8 +1,28 @@
 module Coalmine.Streamer where
 
-import Coalmine.Prelude
+import Coalmine.Prelude hiding (State)
 import qualified Jsonifier
 
 -- |
 -- Specification of how to stream values of a particular type.
-data Streamer a
+newtype Streamer a
+  = Streamer (a -> DataStreamBuilder)
+
+-- |
+-- Efficiently constructs a lazy list that can then be used
+-- as a generator of data.
+buildDataStream :: DataStreamBuilder -> [ByteString]
+buildDataStream = error "TODO"
+
+newtype DataStreamBuilder
+  = DataStreamBuilder (Conf -> State -> Maybe (ByteString, State))
+
+-- |
+-- Configuration of how to stream.
+-- Affects the state update logic.
+data Conf
+  = -- | Chunk size.
+    Conf
+      !Int
+
+data State
