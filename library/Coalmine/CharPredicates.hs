018f7523d3b788b83695a9217359b5412a477a74
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-11-26 20:17:53 +0300

    Add isSpaceOrTab

--- library/Coalmine/CharPredicates.hs
+++ library/Coalmine/CharPredicates.hs
@@ -2,7 +2,10 @@
 -- Indentation-sensitive line-by-line parsing.
 module Coalmine.CharPredicates where
 
-import Coalmine.Prelude
+import Coalmine.Prelude hiding (both, either)
+
+isSpaceOrTab :: Char -> Bool
+isSpaceOrTab = either (== ' ') (== '\t')
 
 -- | Is it a horizontal space character?
 isHSpace :: Char -> Bool
@@ -19,8 +22,10 @@ isAsciiAlpha x = isAscii x && isAlpha x
 
 -- * Combinators
 
+{-# INLINE either #-}
 either :: (Char -> Bool) -> (Char -> Bool) -> Char -> Bool
 either l r x = l x || r x
 
+{-# INLINE both #-}
 both :: (Char -> Bool) -> (Char -> Bool) -> Char -> Bool
 both l r x = l x && r x
