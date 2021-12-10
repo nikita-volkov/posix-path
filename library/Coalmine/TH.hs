8df89ca9453f124b0395b63ae07a36662ec12c0d
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-12-11 01:10:30 +0300

    Move to pureAttoparsedExpQq

--- library/Coalmine/TH.hs
+++ library/Coalmine/TH.hs
@@ -1,13 +1,14 @@
 module Coalmine.TH where
 
 import Coalmine.Prelude
+import qualified Data.Attoparsec.Text as Atto
 import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Syntax
 
-pureExpQq :: (String -> Exp) -> QuasiQuoter
-pureExpQq exp =
+pureAttoparsedExpQq :: Atto.Parser Exp -> QuasiQuoter
+pureAttoparsedExpQq parser =
   QuasiQuoter
-    (pure . exp)
+    (either fail pure . Atto.parseOnly parser . fromString)
     (const (fail "Context unsupported"))
     (const (fail "Context unsupported"))
     (const (fail "Context unsupported"))
