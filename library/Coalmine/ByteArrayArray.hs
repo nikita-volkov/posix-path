3dd68d16535534c7f61b42ef09a864b020d647ca
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-20 18:21:28 +0300

    Implement and simplify

--- library/Coalmine/ByteArrayArray.hs
+++ library/Coalmine/ByteArrayArray.hs
@@ -4,6 +4,7 @@ import Coalmine.Prelude
 import qualified Data.Text as Text
 import qualified Data.Text.Array as TextArray
 import qualified Data.Text.Internal as TextInternal
+import qualified Data.Vector.Unboxed as UVec
 import GHC.Exts
 
 -- *
@@ -15,12 +16,19 @@ import GHC.Exts
 data ByteArrayArray = ByteArrayArray
   { -- | Bytes.
     array :: !ByteArray#,
-    -- | Offsets.
-    offsets :: !(UVec Int)
+    -- | Ends.
+    ends :: !(UVec Int)
   }
 
 -- *
 
+{-# INLINE toTextList #-}
 toTextList :: ByteArrayArray -> [Text]
 toTextList ByteArrayArray {..} =
-  error "TODO"
+  build $ \step start ->
+    let step' end next offset =
+          step (TextInternal.Text textArray offset (end - offset)) (next end)
+        finish _ = start
+     in UVec.foldr step' finish ends 0
+  where
+    textArray = TextArray.Array array
