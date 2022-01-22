d7e5f447f2d9c5d8bc43623c370138cc81418237
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-22 17:10:25 +0300

    Sketch isolated ends

---
+++ library/Coalmine/ByteArrayArray/Ends.hs
@@ -0,0 +1,22 @@
+module Coalmine.ByteArrayArray.Ends where
+
+import Coalmine.Prelude
+import qualified Coalmine.VectorExtras.Generic as GVecExtras
+import qualified Data.Vector.Unboxed as UVec
+import qualified Data.Vector.Unboxed.Mutable as MUVec
+import GHC.Exts
+
+-- *
+
+newtype Ends = Ends (UVec Int)
+
+instance Semigroup Ends where
+  (<>) = error "TODO"
+
+instance Monoid Ends where
+  mempty = Ends mempty
+  mconcat list = Ends $ GVecExtras.initialized totalLength populate
+    where
+      totalLength = getSum $ foldMap (Sum . UVec.length . coerce) $ list
+      populate mv =
+        error "TODO: Map all elements. No hope for vec-copying, since we need to modify it"
