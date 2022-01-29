37044511bdbbb958f72a9f2f65ba2b4b7dc88173
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-29 05:04:59 +0300

    Simplify and implement TH

--- library/Coalmine/Inter/TH.hs
+++ library/Coalmine/Inter/TH.hs
@@ -2,9 +2,38 @@ module Coalmine.Inter.TH where
 
 import qualified Coalmine.Inter.Normalization as N
 import Coalmine.Prelude
+import qualified Data.Text as Text
 import Language.Haskell.TH.Syntax
+import qualified THLego.Helpers as Helpers
 
 -- *
 
-builderExpressions :: BVec N.Line -> [Exp]
-builderExpressions = error "TODO"
+linesExpressions :: BVec N.Line -> [Exp]
+linesExpressions =
+  \vec -> foldr progress finish vec 0 []
+  where
+    progress line next !indent !literalChunks = case line of
+      N.BlankLine -> next 0 ("\n" : literalChunks)
+      N.ContentLine indent segments ->
+        foldr progress next segments indent [Text.replicate indent " "]
+        where
+          progress segment next !indent !literalChunks =
+            case segment of
+              N.PlainContentSegment content ->
+                next indent (content : literalChunks)
+              N.VLineContentSegment ->
+                next indent ("|" : literalChunks)
+              N.PlaceholderContentSegment placeholderName ->
+                placeholder indent placeholderName :
+                literalExps literalChunks <> next indent []
+    finish !indent !literalChunks =
+      literalExps literalChunks
+
+placeholder :: Int -> N.Name -> Exp
+placeholder = error "TODO"
+
+literalExps :: [Text] -> [Exp]
+literalExps literalChunks =
+  case literalChunks of
+    [] -> []
+    _ -> pure $ Helpers.textLitE $ mconcat $ reverse literalChunks
