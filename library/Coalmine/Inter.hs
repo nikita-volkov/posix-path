497d07f3eb507d5a65af5f5fd45e81dcf64ea2a6
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-29 05:21:19 +0300

    Implement Inter

--- library/Coalmine/Inter.hs
+++ library/Coalmine/Inter.hs
@@ -1,3 +1,14 @@
 module Coalmine.Inter where
 
+import qualified Coalmine.Inter.Deindentation as Deindentation
+import qualified Coalmine.Inter.Format.Parsers as Parsers
+import qualified Coalmine.Inter.TH as InterTH
 import Coalmine.Prelude
+import qualified Coalmine.TH as TH
+
+inter :: QuasiQuoter
+inter = TH.pureAttoparsedExpQq parser
+  where
+    parser =
+      Parsers.quasiQuote
+        <&> InterTH.linesExp . Deindentation.quasiQuote
