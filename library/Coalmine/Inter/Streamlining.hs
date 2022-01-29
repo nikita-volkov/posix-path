f9f4f2083b11a9b1d1bffdd0a7ca0a137c893f3e
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-29 04:26:04 +0300

    Implement streamlining

--- library/Coalmine/Inter/Streamlining.hs
+++ library/Coalmine/Inter/Streamlining.hs
@@ -1,14 +1,20 @@
 module Coalmine.Inter.Streamlining where
 
-import qualified Coalmine.Inter.Normalization as N
 import qualified Coalmine.Inter.Structures.LiteralConstructor as LiteralConstructor
 import Coalmine.Prelude
+import qualified Data.Text as Text
 import qualified Data.Vector as BVec
 
 -- *
 
+data Cmd
+  = ReturnCarriageCmd !Int
+  | PrintCmd !Text
+  | AddPlaceholder !Text
+
 data Symbol
-  = LiteralSymbol !Text
+  = LiteralSymbol
+      !Text
   | PlaceholderSymbol
       !Int
       -- ^ Indentation.
@@ -16,14 +22,23 @@ data Symbol
 
 -- *
 
-newtype Streamliner
-  = Streamliner LiteralConstructor.LiteralConstructor
-
-returnCarriage :: Int -> Streamliner -> Streamliner
-returnCarriage = error "TODO"
-
-print :: Text -> Streamliner -> Streamliner
-print = error "TODO"
-
-addPlaceholder :: Text -> Streamliner -> ([Symbol], Streamliner)
-addPlaceholder = error "TODO"
+transduce :: [Cmd] -> [Symbol]
+transduce =
+  go LiteralConstructor.start
+  where
+    go !litCons = \case
+      cmd : tail -> case cmd of
+        ReturnCarriageCmd indent ->
+          go (LiteralConstructor.returnCarriage indent litCons) tail
+        PrintCmd text ->
+          go (LiteralConstructor.addLineContent text litCons) tail
+        AddPlaceholder placeholder ->
+          let (indent, text) = LiteralConstructor.finish litCons
+           in if Text.null text
+                then
+                  PlaceholderSymbol indent placeholder :
+                  go LiteralConstructor.start tail
+                else
+                  LiteralSymbol text :
+                  PlaceholderSymbol indent placeholder :
+                  go LiteralConstructor.start tail
--- library/Coalmine/Inter/TH.hs
+++ library/Coalmine/Inter/TH.hs
@@ -1,7 +1,10 @@
 module Coalmine.Inter.TH where
 
-import qualified Coalmine.Inter.Normalization as Normalization
+import qualified Coalmine.Inter.Normalization as N
 import Coalmine.Prelude
 import Language.Haskell.TH.Syntax
 
 -- *
+
+builderExpressions :: BVec N.Line -> [Exp]
+builderExpressions = error "TODO"
