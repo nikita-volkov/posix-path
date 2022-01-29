cf5c0b6c3833160fdc210b44c45831145c220aae
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-29 03:45:39 +0300

    Implement things and simplify

--- library/Coalmine/Inter/Structures/LiteralConstructor.hs
+++ library/Coalmine/Inter/Structures/LiteralConstructor.hs
@@ -3,6 +3,7 @@ module Coalmine.Inter.Structures.LiteralConstructor where
 import qualified Coalmine.Inter.Normalization as N
 import qualified Coalmine.MultilineTextBuilder as B
 import Coalmine.Prelude
+import qualified Data.Text as Text
 import Language.Haskell.TH.Syntax
 import qualified THLego.Helpers as Helpers
 
@@ -13,20 +14,28 @@ data LiteralConstructor
       !Int
       -- ^ Current line local indent.
       ![Text]
-      -- ^ Current line chunks.
-      ![Text]
-      -- ^ Preceding lines.
+      -- ^ Current chunks.
 
 -- *
 
-start :: Int -> LiteralConstructor
-start = error "TODO"
+start :: LiteralConstructor
+start = LiteralConstructor 0 []
 
-finish :: LiteralConstructor -> Text
-finish (LiteralConstructor indent chunks lines) =
-  error "TODO"
+-- |
+-- Finish, producing the indentation level of the last line.
+finish :: LiteralConstructor -> (Int, Text)
+finish (LiteralConstructor indent chunks) =
+  let content = mconcat $ reverse chunks
+   in (indent, content)
 
 -- *
 
 returnCarriage :: Int -> LiteralConstructor -> LiteralConstructor
-returnCarriage indent = error "TODO"
+returnCarriage indent (LiteralConstructor _ chunks) =
+  let indentText = Text.replicate indent " "
+      chunks' = indentText : "\n" : chunks
+   in LiteralConstructor indent chunks'
+
+addLineContent :: Text -> LiteralConstructor -> LiteralConstructor
+addLineContent text (LiteralConstructor indent chunks) =
+  LiteralConstructor indent (text : chunks)
