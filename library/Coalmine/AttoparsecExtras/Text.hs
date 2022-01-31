d904a264bf92c435e8a7a5fc62daa2429b997194
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-31 21:31:22 +0300

    Add greedy parser

--- library/Coalmine/AttoparsecExtras/Text.hs
+++ library/Coalmine/AttoparsecExtras/Text.hs
@@ -4,8 +4,8 @@ import qualified Coalmine.BaseExtras.Integer as IntegerExtras
 import Coalmine.Prelude
 import Data.Attoparsec.Text
 
-invulnerableUnsignedDecimal :: (Integral a, Show a) => a -> a -> Parser a
-invulnerableUnsignedDecimal min max =
+nonGreedyUnsignedDecimal :: (Integral a, Show a) => a -> a -> Parser a
+nonGreedyUnsignedDecimal min max =
   runScanner (0, 0) step >>= postCheck . snd
   where
     step (i, acc) char =
@@ -49,3 +49,60 @@ invulnerableUnsignedDecimal min max =
       IntegerExtras.countDigits max
     minLen =
       IntegerExtras.countDigits min
+
+greedyUnsignedDecimal :: (Integral a, Show a) => a -> a -> Parser a
+greedyUnsignedDecimal min max =
+  runScanner (0, 0) step >>= postCheck . snd
+  where
+    step (i, acc) char =
+      if i <= maxLen && isDigit char
+        then case acc * 10 + fromIntegral (ord char - 48) of
+          acc -> Just (succ i, acc)
+        else Nothing
+    postCheck (i, acc) =
+      if i <= maxLen
+        then
+          if i >= minLen
+            then
+              if acc <= max
+                then
+                  if acc >= min
+                    then return acc
+                    else
+                      fail $
+                        mconcat
+                          [ "Decimal is smaller than the expected minimum of ",
+                            show min,
+                            ": ",
+                            show acc
+                          ]
+                else
+                  fail $
+                    mconcat
+                      [ "Decimal is larger than the expected maximum of ",
+                        show max,
+                        ": ",
+                        show acc
+                      ]
+            else
+              fail $
+                mconcat
+                  [ "Decimal is shorter than the expected minimum length of ",
+                    show minLen,
+                    ". It is ",
+                    show i,
+                    " characters long"
+                  ]
+        else
+          fail $
+            mconcat
+              [ "Decimal is longer than the expected maximum length of ",
+                show maxLen,
+                ". It is at least ",
+                show i,
+                " characters long"
+              ]
+    maxLen =
+      IntegerExtras.countDigits max
+    minLen =
+      IntegerExtras.countDigits max
