74373907915987d8e9660b5090fb0fb18a38e49c
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-01 17:59:31 +0300

    Update building

--- build.bash
+++ build.bash
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -eo pipefail
 
+function build_and_test {
+  stack build \
+  --test \
+  --fast
+
+  ./.stack-work/dist/x86_64-osx/Cabal-3.4.1.0/build/doctest/doctest
+}
+
+function build {
+  stack build \
+  --fast
+}
+
 ormolu --mode inplace -ce \
 $(find . -name "*.hs" \
   -not -path "./*.stack-work/*" \
   -not -path "./.git/*")
 
-stack build \
---fast
-
-./.stack-work/dist/x86_64-osx/Cabal-3.4.1.0/build/doctest/doctest
+build_and_test
