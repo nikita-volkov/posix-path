f80887fc8b7e66b3fce86220c24b7fba5ceddde4
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-04 14:49:14 +0300

    Add tasty reexports

---
+++ library/Coalmine/Tasty.hs
@@ -0,0 +1,8 @@
+-- |
+-- Reexports for the tasty modules.
+module Coalmine.Tasty (module Exports) where
+
+import Test.QuickCheck.Instances as Exports
+import Test.Tasty as Exports
+import Test.Tasty.HUnit as Exports
+import Test.Tasty.QuickCheck as Exports
