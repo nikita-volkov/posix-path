e6fe77263d4a7ba9a33b3e7b5be5d0609283943c
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-10 20:15:24 +0300

    Add Operators

---
+++ library/Coalmine/Operators.hs
@@ -0,0 +1,50 @@
+module Coalmine.Operators
+  ( module Exports,
+  )
+where
+
+import Control.Applicative as Exports
+  ( (*>),
+    (<*),
+    (<*>),
+    (<|>),
+  )
+import Control.Monad as Exports
+  ( (<=<),
+    (=<<),
+    (>=>),
+    (>>),
+    (>>=),
+  )
+import Data.Eq as Exports
+  ( (/=),
+    (==),
+  )
+import Data.Function as Exports
+  ( ($),
+    (&),
+    (.),
+  )
+import Data.Functor as Exports
+  ( ($>),
+    (<$),
+    (<$>),
+    (<&>),
+  )
+import Data.Ord as Exports
+  ( (<),
+    (<=),
+    (>),
+    (>=),
+  )
+import Data.Semigroup as Exports
+  ( (<>),
+  )
+import GHC.Num as Exports
+  ( (*),
+    (+),
+    (-),
+  )
+import GHC.Real as Exports
+  ( (/),
+  )
