eccaeba7f0cea23667741be44e0b8519b7845c89
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-22 00:20:46 +0300

    Add apply

--- library/Coalmine/InternalPrelude/Function.hs
+++ library/Coalmine/InternalPrelude/Function.hs
@@ -2,6 +2,10 @@ module Coalmine.InternalPrelude.Function where
 
 import Prelude
 
+apply :: a -> (a -> b) -> b
+apply arg fn =
+  fn arg
+
 applyAll :: [a -> b] -> a -> [b]
 applyAll fnList arg =
   fmap ($ arg) fnList
