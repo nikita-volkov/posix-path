8b1ac5617b74a296efba906be5dabc282218144d
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-21 19:51:13 +0300

    Add headed megaparsec testing helpers

---
+++ library/Coalmine/Tasty/HeadedMegaparsec.hs
@@ -0,0 +1,48 @@
+module Coalmine.Tasty.HeadedMegaparsec where
+
+import qualified Coalmine.HeadedMegaparsecExtras as P
+import Coalmine.Prelude hiding (FilePath)
+import Coalmine.SimplePaths (FilePath)
+import HeadedMegaparsec
+import Test.QuickCheck.Instances
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import qualified Text.Megaparsec as Megaparsec
+
+testParserToSucceed ::
+  (Eq a, Show a, Megaparsec.VisualStream i, Megaparsec.TraversableStream i, Megaparsec.ShowErrorComponent e) =>
+  TestName ->
+  a ->
+  HeadedParsec e i a ->
+  i ->
+  TestTree
+testParserToSucceed name expectation parser input =
+  testCase name $ case P.toRefiner parser input of
+    Right res -> assertEqual "" expectation res
+    Left err -> assertFailure . toString $ err
+
+testParserToFail ::
+  (Eq a, Show a, Megaparsec.VisualStream i, Megaparsec.TraversableStream i, Megaparsec.ShowErrorComponent e) =>
+  TestName ->
+  Text ->
+  HeadedParsec e i a ->
+  i ->
+  TestTree
+testParserToFail name expectation parser input =
+  testCase name $ case P.toRefiner parser input of
+    Right res -> assertFailure . show $ res
+    Left err -> assertEqual "" expectation err
+
+testFileParserToSucceed ::
+  (Eq a, Show a) =>
+  TestName ->
+  FilePath ->
+  HeadedParsec Void Text a ->
+  a ->
+  TestTree
+testFileParserToSucceed name path parser expectation =
+  testCase name $
+    P.toFileReader parser path >>= \case
+      Right res -> assertEqual "" expectation res
+      Left err -> assertFailure . toString $ err
