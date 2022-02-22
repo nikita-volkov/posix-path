cd094ae63b29d1ea105d0cff4e897a883f06d56d
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-22 16:45:12 +0300

    Extend the tests

--- test/TestSuites/LocatedRendering.hs
+++ test/TestSuites/LocatedRendering.hs
@@ -7,21 +7,35 @@ import Coalmine.Tasty
 import qualified Data.Text as Text
 
 tests =
-  [ testCase "select" $
-      let input =
-            [i|
-              "id" int8 not null null generated always as identity primary key,
-              "name" text not null unique
-            |]
-          actualResult = Text.intercalate "\n" $ Rendering.select 2 19 6 $ Text.lines input
-          expectedResult =
-            [i|
-              2 | "id" int8 not null null generated always as identity primary key,
-                |                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-              3 | "name" text not null unique
-                | ^^^^^^
-            |]
-       in assertEqual (toString actualResult) expectedResult actualResult,
+  [ testGroup "select" $
+      [ testCase "multiline" $
+          let input =
+                [i|
+                  "id" int8 not null null generated always as identity primary key,
+                  "name" text not null unique
+                |]
+              actualResult = Text.intercalate "\n" $ Rendering.select 2 19 6 $ Text.lines input
+              expectedResult =
+                [i|
+                  2 | "id" int8 not null null generated always as identity primary key,
+                    |                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                  3 | "name" text not null unique
+                    | ^^^^^^
+                |]
+           in assertEqual (toString actualResult) expectedResult actualResult,
+        testCase "single-line" $
+          let input =
+                [i|
+                  "id" int8 not null null generated always as identity primary key,
+                |]
+              actualResult = Text.intercalate "\n" $ Rendering.select 2 19 23 $ Text.lines input
+              expectedResult =
+                [i|
+                  2 | "id" int8 not null null generated always as identity primary key,
+                    |                    ^^^^
+                |]
+           in assertEqual (toString actualResult) expectedResult actualResult
+      ],
     testCase "single-line" $
       let input =
             [i|
