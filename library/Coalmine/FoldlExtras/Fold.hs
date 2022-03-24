1c775947f25efb92aa088b50158c712e68e83c2f
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-03-24 16:33:01 +0300

    Sketch discretize

--- library/Coalmine/FoldlExtras/Fold.hs
+++ library/Coalmine/FoldlExtras/Fold.hs
@@ -2,3 +2,18 @@ module Coalmine.FoldlExtras.Fold where
 
 import Coalmine.Prelude
 import Control.Foldl (Fold (..))
+
+discretize :: Int -> Int -> (a -> Int) -> (a -> b) -> Fold b o -> Fold a o
+discretize distance initEndPosition toPosition toOutput (Fold step init extract) =
+  Fold step' init' extract'
+  where
+    init' = (initEndPosition, init)
+    step' (endPosition, acc) input =
+      if endPosition > position
+        then error "TODO"
+        else error "TODO"
+      where
+        position =
+          toPosition input
+    extract' =
+      error "TODO"
