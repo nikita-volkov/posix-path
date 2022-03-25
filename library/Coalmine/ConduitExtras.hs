886a2234f26ff99c513d2546a5c08f15c8c94e7c
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-03-25 22:07:57 +0300

    Implement mapping moore

--- library/Coalmine/ConduitExtras.hs
+++ library/Coalmine/ConduitExtras.hs
@@ -1,13 +1,13 @@
 module Coalmine.ConduitExtras where
 
-import Coalmine.Prelude
+import Coalmine.Prelude hiding (yield)
 import Conduit
 import qualified Control.Foldl as Foldl
 import qualified Data.Machine.Moore as MachinesMoore
 
 -- *
 
-fold :: Monad m => Foldl.Fold i r -> Conduit.ConduitT i o m r
+fold :: Monad m => Foldl.Fold i r -> ConduitT i o m r
 fold (Foldl.Fold progress start finish) =
   go start
   where
@@ -16,8 +16,14 @@ fold (Foldl.Fold progress start finish) =
         Just i -> go (progress state i)
         Nothing -> pure $ finish state
 
-moore :: Monad m => MachinesMoore.Moore i r -> Conduit.ConduitT i o m r
-moore (MachinesMoore.Moore terminate next) =
+reducingMoore :: Monad m => MachinesMoore.Moore i r -> ConduitT i o m r
+reducingMoore (MachinesMoore.Moore terminate next) =
   await >>= \case
-    Just i -> moore $ next i
+    Just i -> reducingMoore $ next i
     Nothing -> pure terminate
+
+mappingMoore :: Monad m => MachinesMoore.Moore i o -> ConduitT i o m ()
+mappingMoore (MachinesMoore.Moore emit next) =
+  yield emit >> await >>= \case
+    Just i -> mappingMoore $ next i
+    Nothing -> pure ()
