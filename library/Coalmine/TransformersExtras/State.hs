ad22b2fa8221811d106d725738d45c2c48b663e7
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-03-29 12:06:45 +0300

    Add unlessMissing

--- library/Coalmine/TransformersExtras/State.hs
+++ library/Coalmine/TransformersExtras/State.hs
@@ -5,3 +5,12 @@ import Coalmine.Prelude
 liftModifyM :: Functor m => (s -> m s) -> StateT s m ()
 liftModifyM modify =
   StateT $ fmap (fmap pure) modify
+
+unlessMissing :: (MonadError e m) => e -> StateT s m a -> StateT (Maybe s) m a
+unlessMissing missingErr m =
+  StateT $ \case
+    Just x -> do
+      (r, x) <- runStateT m x
+      return (r, Just x)
+    Nothing ->
+      throwError missingErr
