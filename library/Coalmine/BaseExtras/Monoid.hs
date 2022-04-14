8c4fe2f6829885730eafa571a572d18f56789edf
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-04-14 18:23:26 +0300

    Add Monoid.filtered

---
+++ library/Coalmine/BaseExtras/Monoid.hs
@@ -0,0 +1,8 @@
+module Coalmine.BaseExtras.Monoid where
+
+import Coalmine.InternalPrelude
+
+filtered :: Monoid m => (a -> Bool) -> (a -> m) -> a -> m
+filtered p k x
+  | p x = k x
+  | otherwise = mempty
