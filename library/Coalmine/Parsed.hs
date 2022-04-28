9c3d98aa729d8ca2a3a7f4fd0ac3d517230d532b
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-04-28 19:18:44 +0300

    Sketch Parsed

---
+++ library/Coalmine/Parsed.hs
@@ -0,0 +1,47 @@
+module Coalmine.Parsed
+  ( -- *
+    Parsed (..),
+    analyse,
+    renderInMegaparsecStyle,
+  )
+where
+
+import Coalmine.InternalPrelude
+import qualified Coalmine.Located as Located
+
+-- *
+
+-- |
+-- Value accompanied by a reference to a snipped of source code.
+-- 
+-- Useful for reference-maintaining code analysis.
+-- 
+-- A more complete abstraction than 'Located.Located'.
+data Parsed a
+  = Parsed
+      !Text
+      -- ^ Reference to the source text.
+      !(Located.Located a)
+  deriving (Functor, Show, Eq, Foldable, Traversable)
+
+instance Applicative Parsed where
+  pure = Parsed "" . pure
+  Parsed _ lLoc <*> Parsed rSrc rLoc =
+    Parsed rSrc (lLoc <*> rLoc)
+
+instance Comonad Parsed where
+  extract (Parsed _ loc) = extract loc
+  duplicate (Parsed src loc) = Parsed src (fmap (Parsed src . pure) loc)
+
+-- |
+-- Process using a provided pure refinement function.
+analyse :: Parsed a -> (a -> Either e b) -> Either (Parsed e) b
+analyse (Parsed src loc) mapper =
+  first (Parsed src) (Located.analyse loc mapper)
+
+-- |
+-- Pretty-print an error message, asssociating it with the input,
+-- Megaparsec-style.
+renderInMegaparsecStyle :: Parsed Text -> Text
+renderInMegaparsecStyle (Parsed input located) =
+  Located.renderInMegaparsecStyle located input
