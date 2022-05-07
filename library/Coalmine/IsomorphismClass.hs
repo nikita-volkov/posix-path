1ae848bd0d29e1cca6980b26ed0eebe6a1267c99
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-05-07 15:23:14 +0300

    Add docs and instances

--- library/Coalmine/IsomorphismClass.hs
+++ library/Coalmine/IsomorphismClass.hs
@@ -17,13 +17,48 @@ import qualified Data.Text.Lazy.Builder as TextLazyBuilder
 -- The law is:
 --
 -- @'from' . 'to' = 'id'@
+--
+-- This class is particularly easy to use in combination with the @TypeApplications@ extension
+-- making it clear to the reader what sort of conversion he sees. E.g.,
+--
+-- > fromString = from @String
+--
+-- > toText = to @Text
+--
+-- The types are also self-evident:
+--
+-- > > :t from @String
+-- > from @String :: IsomorphicTo String b => String -> b
+--
+-- > > :t to @Text
+-- > to @Text :: IsomorphicTo Text b => b -> Text
 class IsomorphicTo a b where
   to :: b -> a
   from :: a -> b
 
+instance IsomorphicTo String String where
+  to = id
+  from = id
+
 instance IsomorphicTo String Text where
   to = fromText
   from = toText
+
+instance IsomorphicTo String TextBuilder where
+  to = from @Text . to @Text
+  from = from @Text . to @Text
+
+instance IsomorphicTo String TextLazy.Text where
+  to = TextLazy.unpack
+  from = TextLazy.pack
+
+instance IsomorphicTo String TextLazyBuilder.Builder where
+  to = TextLazy.unpack . TextLazyBuilder.toLazyText
+  from = TextLazyBuilder.fromString
+
+instance IsomorphicTo Text Text where
+  to = id
+  from = id
 
 instance IsomorphicTo Text String where
   to = toText
@@ -41,13 +76,37 @@ instance IsomorphicTo Text TextLazyBuilder.Builder where
   to = toText
   from = fromText
 
+instance IsomorphicTo TextBuilder TextBuilder where
+  to = id
+  from = id
+
+instance IsomorphicTo TextBuilder String where
+  to = from @String
+  from = to @String
+
 instance IsomorphicTo TextBuilder Text where
-  to = fromText
-  from = toText
+  to = toTextBuilder
+  from = fromTextBuilder
+
+instance IsomorphicTo TextLazy.Text TextLazy.Text where
+  to = id
+  from = id
+
+instance IsomorphicTo TextLazy.Text String where
+  to = fromString
+  from = TextLazy.unpack
 
 instance IsomorphicTo TextLazy.Text Text where
   to = fromText
   from = toText
+
+instance IsomorphicTo TextLazyBuilder.Builder TextLazyBuilder.Builder where
+  to = id
+  from = id
+
+instance IsomorphicTo TextLazyBuilder.Builder String where
+  to = fromString
+  from = to . to @Text
 
 instance IsomorphicTo TextLazyBuilder.Builder Text where
   to = fromText
