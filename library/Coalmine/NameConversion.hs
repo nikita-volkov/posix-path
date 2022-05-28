bedf630b29ea3aeb8d20174fbeb6dacaa6f68860
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-05-28 12:10:16 +0300

    Add IsLabel instances for usability

--- library/Coalmine/NameConversion.hs
+++ library/Coalmine/NameConversion.hs
@@ -4,12 +4,28 @@ import Coalmine.InternalPrelude
 import qualified Coalmine.MultilineTextBuilder as MultilineTextBuilder
 import Coalmine.Name
 
+-- * --
+
 -- | Name rendering letter case.
 data FromNameCase
   = SpinalFromNameCase
   | SnakeFromNameCase
   | UpperCamelFromNameCase
   | LowerCamelFromNameCase
+
+instance IsLabel "spinal" FromNameCase where
+  fromLabel = SpinalFromNameCase
+
+instance IsLabel "snake" FromNameCase where
+  fromLabel = SnakeFromNameCase
+
+instance IsLabel "upperCamel" FromNameCase where
+  fromLabel = UpperCamelFromNameCase
+
+instance IsLabel "lowerCamel" FromNameCase where
+  fromLabel = LowerCamelFromNameCase
+
+-- * --
 
 class FromName a where
   fromName :: FromNameCase -> Name -> a
