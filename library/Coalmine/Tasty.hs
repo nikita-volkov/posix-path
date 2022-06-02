390314564f6f2bc20cb83c4e3c5e0f96420099ad
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-02 14:57:53 +0300

    Add eqTestCase

--- library/Coalmine/Tasty.hs
+++ library/Coalmine/Tasty.hs
@@ -1,8 +1,17 @@
 -- |
 -- Reexports for the tasty modules.
-module Coalmine.Tasty (module Exports) where
+module Coalmine.Tasty
+  ( module Exports,
+    eqTestCase,
+  )
+where
 
+import Coalmine.InternalPrelude
 import Test.QuickCheck.Instances as Exports
 import Test.Tasty as Exports
 import Test.Tasty.HUnit as Exports
 import Test.Tasty.QuickCheck as Exports
+
+eqTestCase :: (Eq a, Show a) => TestName -> a -> a -> TestTree
+eqTestCase name expected actual =
+  testCase name $ assertEqual "" expected actual
