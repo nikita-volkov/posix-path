103356be6014531a28bef88a7fad880cfe74e972
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-11 03:02:38 +0300

    Implement rest easy

--- library/Coalmine/RestEasy.hs
+++ library/Coalmine/RestEasy.hs
@@ -5,6 +5,7 @@ import qualified Coalmine.BaseExtras.List as List
 import Coalmine.InternalPrelude
 import Coalmine.Parsing
 import qualified Data.Attoparsec.Text as Attoparsec
+import qualified Data.ByteString as ByteString
 import qualified Data.Serialize as Cereal
 import qualified Data.Text as Text
 import qualified Data.Vector as BVec
@@ -30,19 +31,36 @@ serve =
 
 -- |
 -- Request body parser.
-data RequestBody i
+data RequestBody a
+  = RequestBody
+      !Text
+      -- ^ Expected type.
+      (IO ByteString -> IO (Either Text a))
 
 instance Functor RequestBody
 
-jsonRequestBody :: AesonValueParser.Value i -> RequestBody i
+jsonRequestBody :: AesonValueParser.Value a -> RequestBody a
 jsonRequestBody =
   error "TODO"
 
 binaryRequestBody :: Cereal.Get a -> RequestBody a
-binaryRequestBody =
-  error "TODO"
+binaryRequestBody get =
+  RequestBody "application/binary" run
+  where
+    run loadChunk =
+      go $ Cereal.runGetPartial get
+      where
+        go decode = do
+          chunk <- loadChunk
+          if ByteString.null chunk
+            then return $ Left "Not enough data"
+            else case decode chunk of
+              Cereal.Done res _ -> return $ Right res
+              Cereal.Fail err _ -> return $ Left $ to err
+              Cereal.Partial decodeNext -> go decodeNext
 
-data Route
+newtype Route
+  = Route ([Text] -> Wai.Request -> IO Wai.Response)
 
 postRoute :: [RequestBody req] -> (req -> IO Response) -> Route
 postRoute =
