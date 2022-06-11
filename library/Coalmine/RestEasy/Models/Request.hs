ecf703a4eb37e5fe077d7a0e6b6e07ff0a262ddc
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-11 15:25:14 +0300

    Sketch request model

---
+++ library/Coalmine/RestEasy/Models/Request.hs
@@ -0,0 +1,21 @@
+module Coalmine.RestEasy.Models.Request where
+
+import Coalmine.InternalPrelude
+import qualified Domain
+import qualified DomainOptics
+
+Domain.declare
+  Nothing
+  ( mconcat
+      [ Domain.enumDeriver,
+        Domain.boundedDeriver,
+        Domain.showDeriver,
+        Domain.eqDeriver,
+        Domain.ordDeriver,
+        Domain.genericDeriver,
+        Domain.accessorIsLabelDeriver,
+        Domain.constructorIsLabelDeriver,
+        DomainOptics.labelOpticDeriver
+      ]
+  )
+  =<< Domain.loadSchema "domain/rest-easy-request.domain.yaml"
