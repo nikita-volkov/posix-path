975f7ad37bbceb1df30a1b48c48e6353c7699664
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-14 14:04:25 +0300

    Make the cereal test expect complete input

--- library/Coalmine/Tasty/TestTrees/Cereal.hs
+++ library/Coalmine/Tasty/TestTrees/Cereal.hs
@@ -1,5 +1,6 @@
 module Coalmine.Tasty.TestTrees.Cereal where
 
+import qualified Coalmine.CerealExtras.Get as Get
 import Coalmine.InternalPrelude
 import qualified Data.Serialize as Cereal
 import Test.QuickCheck.Instances
@@ -10,4 +11,4 @@ import Test.Tasty.QuickCheck
 testEncodeDecode :: (Arbitrary a, Eq a, Show a, Cereal.Serialize a) => Proxy a -> TestTree
 testEncodeDecode proxy =
   testProperty "Encode-decode equals original" $ \a ->
-    Right (asProxyTypeOf a proxy) === Cereal.decode (Cereal.encode a)
+    Right (asProxyTypeOf a proxy) === Get.runCompletely Cereal.get (Cereal.encode a)
