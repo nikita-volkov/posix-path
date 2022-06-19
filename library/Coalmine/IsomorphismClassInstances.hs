80c955a9dfabca99a12409286df5e6068a486224
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-19 14:45:46 +0300

    Extract isomorphism class instances of text-builder-dev

--- library/Coalmine/IsomorphismClassInstances.hs
+++ library/Coalmine/IsomorphismClassInstances.hs
@@ -5,9 +5,6 @@ module Coalmine.IsomorphismClassInstances where
 import Coalmine.InternalPrelude
 import qualified Coalmine.TimeExtras.Conversions as TimeConversions
 import Data.Ratio ((%))
-import qualified Data.Text as Text
-import qualified Data.Text.Lazy as TextLazy
-import qualified Data.Text.Lazy.Builder as TextLazyBuilder
 import qualified Data.Vector.Generic as VectorGeneric
 import qualified Data.Vector.Unboxed as VectorUnboxed
 import qualified TextBuilderDev
@@ -25,26 +22,6 @@ instance IsomorphicTo (Deque a) (BVec a) where
 
 --
 
-instance IsomorphicTo TextBuilder TextBuilder where
-  to = id
-
-instance IsomorphicTo TextBuilder String where
-  to = TextBuilderDev.string
-
-instance IsomorphicTo TextBuilder Text where
-  to = TextBuilderDev.text
-
-instance IsomorphicTo TextBuilder TextLazy.Text where
-  to = TextBuilderDev.lazyText
-
-instance IsomorphicTo TextBuilder TextLazyBuilder.Builder where
-  to = to . to @TextLazy.Text
-
---
-
-instance IsomorphicTo String TextBuilder where
-  to = to . to @Text
-
 instance IsomorphicTo [a] (Deque a) where
   to = toList
 
@@ -52,18 +29,3 @@ instance IsomorphicTo [a] (Deque a) where
 
 instance IsomorphicTo (BVec a) (Deque a) where
   to = to . to @[a]
-
---
-
-instance IsomorphicTo Text TextBuilder where
-  to = TextBuilderDev.buildText
-
---
-
-instance IsomorphicTo TextLazy.Text TextBuilder where
-  to = to . to @Text
-
---
-
-instance IsomorphicTo TextLazyBuilder.Builder TextBuilder where
-  to = to . to @Text
