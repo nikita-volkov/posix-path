43e22a08f532e8279c784852ef0a213f7aeb1d37
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-22 07:54:56 +0300

    Reuse suchThat

--- library/Coalmine/QuickCheckExtras/Gens.hs
+++ library/Coalmine/QuickCheckExtras/Gens.hs
@@ -4,10 +4,4 @@ import Coalmine.InternalPrelude
 import Test.QuickCheck.Gen
 
 filter :: (a -> Bool) -> Gen a -> Gen a
-filter pred gen = go
-  where
-    go = do
-      a <- gen
-      if pred a
-        then return a
-        else go
+filter = flip suchThat
