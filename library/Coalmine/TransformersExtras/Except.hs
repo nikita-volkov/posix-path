0e243b933d5166b2c51e59f3419d7e8592447c3d
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-07-14 22:20:11 +0300

    Add IsString for ExceptT

--- library/Coalmine/TransformersExtras/Except.hs
+++ library/Coalmine/TransformersExtras/Except.hs
@@ -8,3 +8,6 @@ instance (Semigroup a, Monad m) => Semigroup (ExceptT r m a) where
 
 instance (Monoid a, Monad m) => Monoid (ExceptT r m a) where
   mempty = pure mempty
+
+instance (IsString a, Monad m) => IsString (ExceptT r m a) where
+  fromString = return . fromString
