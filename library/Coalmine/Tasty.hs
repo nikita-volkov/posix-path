ab6c6aedc171ee0f36c6ee5ca28f4c2679782dd2
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-07-20 17:15:44 +0300

    Add suitesMain

--- library/Coalmine/Tasty.hs
+++ library/Coalmine/Tasty.hs
@@ -1,6 +1,11 @@
--- |
--- Reexports for the tasty modules.
-module Coalmine.Tasty (module Exports) where
+module Coalmine.Tasty
+  ( -- * Reexports of the original Tasty modules.
+    module Exports,
+
+    -- * Extensions
+    suitesMain,
+  )
+where
 
 import Coalmine.InternalPrelude
 import Coalmine.Tasty.TestTrees as Exports
@@ -8,3 +13,11 @@ import Test.QuickCheck.Instances as Exports
 import Test.Tasty as Exports
 import Test.Tasty.HUnit as Exports
 import Test.Tasty.QuickCheck as Exports
+
+-- | Individually initialized test trees.
+--
+-- Helpful for uniting multiple modules.
+suitesMain :: [IO TestTree] -> IO ()
+suitesMain suites =
+  defaultMain . testGroup "Suites"
+    =<< sequence suites
