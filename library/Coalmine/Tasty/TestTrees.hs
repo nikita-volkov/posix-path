a7449f71304cfbc86d2590519cd3a541468c0f90
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-09-22 20:26:45 +0300

    Add eitherEqTestCase

--- library/Coalmine/Tasty/TestTrees.hs
+++ library/Coalmine/Tasty/TestTrees.hs
@@ -8,3 +8,9 @@ import Test.Tasty.QuickCheck
 eqTestCase :: (Eq a, Show a) => TestName -> a -> a -> TestTree
 eqTestCase name expected actual =
   testCase name $ assertEqual "" expected actual
+
+eitherEqTestCase :: (Eq a, Show a) => TestName -> a -> Either Text a -> TestTree
+eitherEqTestCase name expected actual =
+  testCase name $ case actual of
+    Right actual -> assertEqual "" expected actual
+    Left err -> assertFailure $ to err
