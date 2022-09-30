7f2fb6e6dec8495ef7486a9102674476d3dc9e02
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-09-30 11:09:26 +0300

    Add listSampler

--- library/Coalmine/EvenSimplerPaths/QuickCheckGens.hs
+++ library/Coalmine/EvenSimplerPaths/QuickCheckGens.hs
@@ -5,6 +5,8 @@ import Coalmine.InternalPrelude hiding (filter)
 import Coalmine.QuickCheckExtras.Gens
 import qualified StructureKit.Charset as Charset
 import Test.QuickCheck
+import Test.QuickCheck.Gen
+import qualified Test.QuickCheck.Random as QcRandom
 
 fileName :: Gen Text
 fileName = do
@@ -28,3 +30,20 @@ fileExtensions :: Gen [Text]
 fileExtensions = do
   amount <- chooseInt (0, 10)
   vectorOf amount fileExtension
+
+qcGen :: Gen QcRandom.QCGen
+qcGen =
+  arbitrary <&> QcRandom.mkQCGen
+
+genRunner :: Gen (Gen a -> a)
+genRunner = do
+  genState <- qcGen
+  seed <- arbitrary
+  return $ \(MkGen run) -> run genState seed
+
+listSampler :: Gen ([a] -> [a])
+listSampler = do
+  run <- genRunner
+  return $ \list -> run $ do
+    shuffled <- shuffle list
+    sublistOf shuffled
