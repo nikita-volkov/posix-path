b5816bed010c49af8775c131c037e2790c149548
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-05 10:13:20 +0300

    Add mapTail and mapInit

--- library/Coalmine/ContainersExtras/Seq.hs
+++ library/Coalmine/ContainersExtras/Seq.hs
@@ -8,7 +8,17 @@ mapHead f seq = case viewl seq of
   EmptyL -> mempty
   head :< tail -> f head <| tail
 
+mapTail :: (a -> a) -> Seq a -> Seq a
+mapTail f seq = case viewl seq of
+  EmptyL -> mempty
+  head :< tail -> head <| fmap f tail
+
 mapTailTip :: (a -> a) -> Seq a -> Seq a
 mapTailTip f seq = case viewr seq of
   EmptyR -> mempty
   init :> tailTip -> init |> f tailTip
+
+mapInit :: (a -> a) -> Seq a -> Seq a
+mapInit f seq = case viewr seq of
+  EmptyR -> mempty
+  init :> tailTip -> fmap f init |> tailTip
