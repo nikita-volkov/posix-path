e2488f6b5b867e5c61383e839ba06c9defffde86
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-13 14:42:53 +0300

    Reimplement fileset around map

--- library/Coalmine/Fileset.hs
+++ library/Coalmine/Fileset.hs
@@ -17,21 +17,22 @@ import qualified Coalmine.EvenSimplerPaths as Paths
 import Coalmine.InternalPrelude hiding (print)
 import qualified Coalmine.MultilineTextBuilder as B
 import Coalmine.Printing
+import qualified Data.Map.Strict as Map
 import qualified Data.Text.IO as TextIO
 
 -- * --
 
-newtype Fileset = Fileset [(Path, Text)]
+newtype Fileset = Fileset {map :: Map Path Text}
   deriving (Semigroup, Monoid)
 
 instance IsList Fileset where
   type Item Fileset = (Path, Text)
-  fromList = Fileset
-  toList = coerce
+  fromList = Fileset . fromList
+  toList = toList . (.map)
 
 instance BroadPrinting Fileset where
-  toBroadBuilder (Fileset package) =
-    B.intercalate "\n\n" $ fmap renderFile package
+  toBroadBuilder =
+    B.intercalate "\n\n" . fmap renderFile . toList
     where
       renderFile (path, contents) =
         mconcat
@@ -44,8 +45,8 @@ instance BroadPrinting Fileset where
 
 -- | Write to file system.
 write :: Fileset -> IO ()
-write (Fileset files) =
-  traverse_ (uncurry writeFileCreatingDirs) files
+write =
+  traverse_ (uncurry writeFileCreatingDirs) . toList
   where
     writeFileCreatingDirs path contents = do
       Paths.createDirsTo path
@@ -59,10 +60,10 @@ print = printBroadToStdOut
 
 file :: Path -> Text -> Fileset
 file path content =
-  Fileset [(path, content)]
+  Fileset $ Map.singleton path content
 
 -- |
 -- Prepend a directory path to all contents of this package.
 inDir :: Path -> Fileset -> Fileset
-inDir path (Fileset contents) =
-  Fileset $ fmap (first (mappend path)) contents
+inDir path =
+  Fileset . Map.mapKeys (mappend path) . (.map)
