06e481632d8d9df6bb2815941ec4da6a6972ac9a
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-13 23:57:29 +0300

    Rename things

--- library/Coalmine/Retry.hs
+++ library/Coalmine/Retry.hs
@@ -1,6 +1,46 @@
-module Coalmine.RetryStrategy where
+module Coalmine.Retry where
 
 import Coalmine.InternalPrelude hiding (print)
+
+retryIO ::
+  RetryStrategy ->
+  -- | Action to execute on each attempt.
+  -- When Right the result is propagated.
+  IO (Either err ok) ->
+  -- | Action producing the last attempt error and attempt count in case of
+  -- retry strategy terminating.
+  IO (Either (err, Int) ok)
+retryIO (RetryStrategy retryState retryStep) attempt =
+  go retryState 0
+  where
+    go !retryState !attemptCount =
+      attempt >>= \case
+        Right ok -> return (Right ok)
+        Left err -> do
+          case retryStep retryState of
+            Nothing -> return (Left (err, attemptCount))
+            Just (delayInMilliseconds, retryState) -> do
+              threadDelay $ 1000 * delayInMilliseconds
+              go retryState (succ attemptCount)
+
+retryStatefulIO ::
+  RetryStrategy ->
+  -- | Initial attempt state.
+  state ->
+  -- | Action to execute on each attempt.
+  (state -> IO (Either state ok)) ->
+  IO (Either state ok)
+retryStatefulIO (RetryStrategy retryState retryStep) initialAttemptState attemptStep =
+  go initialAttemptState retryState
+  where
+    go !attemptState !retryState =
+      attemptStep attemptState >>= \case
+        Right ok -> return (Right ok)
+        Left nextAttemptState -> case retryStep retryState of
+          Nothing -> return (Left attemptState)
+          Just (delayInMilliseconds, nextRetryState) -> do
+            threadDelay $ 1000 * delayInMilliseconds
+            go nextAttemptState nextRetryState
 
 data RetryStrategy
   = forall state.
@@ -14,54 +54,15 @@ data RetryStrategy
       -- decide what that means. Typically interpreting it as a fatal error
       -- due to which the app should stop running.
 
-runRetryStrategyInIO ::
-  RetryStrategy ->
-  -- | Action to execute on each attempt.
-  IO (Either err ok) ->
-  -- | Action producing the last attempt error and attempt count in case of
-  -- retry strategy terminating..
-  IO (Either (err, Int) ok)
-runRetryStrategyInIO (RetryStrategy retryState retryStep) attempt =
-  go retryState 0
-  where
-    go !retryState !attemptCount =
-      attempt >>= \case
-        Right ok -> return (Right ok)
-        Left err -> do
-          case retryStep retryState of
-            Nothing -> return (Left (err, attemptCount))
-            Just (delayInMilliseconds, retryState) -> do
-              threadDelay $ 1000 * delayInMilliseconds
-              go retryState (succ attemptCount)
-
-runRetryStrategyInIOStatefully ::
-  RetryStrategy ->
-  -- | Initial attempt state.
-  state ->
-  -- | Action to execute on each attempt.
-  (state -> IO (Either state ok)) ->
-  IO (Either state ok)
-runRetryStrategyInIOStatefully (RetryStrategy retryState retryStep) initialAttemptState attemptStep =
-  go initialAttemptState retryState
-  where
-    go !attemptState !retryState =
-      attemptStep attemptState >>= \case
-        Right ok -> return (Right ok)
-        Left nextAttemptState -> case retryStep retryState of
-          Nothing -> return (Left attemptState)
-          Just (delayInMilliseconds, nextRetryState) -> do
-            threadDelay $ 1000 * delayInMilliseconds
-            go nextAttemptState nextRetryState
-
 -- | Execute an iteration of the strategy,
 -- producing a strategy for the next iteration.
-step :: RetryStrategy -> Maybe (Int, RetryStrategy)
-step (RetryStrategy state step) =
+stepRetryStrategy :: RetryStrategy -> Maybe (Int, RetryStrategy)
+stepRetryStrategy (RetryStrategy state step) =
   case step state of
     Just (emission, state) -> Just (emission, RetryStrategy state step)
     Nothing -> Nothing
 
-growFromToByFactor ::
+growFromToByFactorRetryStrategy ::
   -- | Initial amount of milliseconds.
   Int ->
   -- | Max milliseconds. Inclusive.
@@ -69,7 +70,7 @@ growFromToByFactor ::
   -- | Factor.
   Double ->
   RetryStrategy
-growFromToByFactor init max factor =
+growFromToByFactorRetryStrategy init max factor =
   RetryStrategy init step
   where
     step lastMillis =
