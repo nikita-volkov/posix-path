90e06ec22fb4e033e43bcb8c64df8bdd86806d54
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-19 19:25:12 +0300

    Sketch Lingua

---
+++ library/Coalmine/Lingua.hs
@@ -0,0 +1,43 @@
+-- | Classes for various conversion between data-structures and general operations.
+module Coalmine.Lingua where
+
+import Coalmine.InternalPrelude
+import qualified Data.Text
+import qualified Data.Text.Lazy
+import qualified Data.Text.Lazy.Builder
+
+type family Strict a
+
+type instance
+  Strict Data.Text.Lazy.Text =
+    Data.Text.Text
+
+class CanBeStrict a where
+  toStrict :: a -> Strict a
+
+type family Lazy a
+
+class CanBeLazy a where
+  toLazy :: a -> Lazy a
+
+-- | A finalized representation of some structure that is
+-- intended for modification.
+--
+-- Usually such data-structure is better optimized for read-operations.
+-- E.g., accessing metainformation like length.
+type family Frozen a
+
+type instance
+  Frozen Data.Text.Text =
+    Data.Text.Lazy.Builder.Builder
+
+class Freezes a where
+  freeze :: a -> Frozen a
+
+type family Melted a
+
+class Melts a where
+  melt :: a -> Melted a
+
+class HasLength a where
+  length :: a -> Int
