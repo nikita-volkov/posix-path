e4fe82a58d880b524612bbc90e695107b62b37cc
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-21 10:26:44 +0300

    Add builders

--- library/Coalmine/IsomorphismClasses.hs
+++ library/Coalmine/IsomorphismClasses.hs
@@ -11,6 +11,8 @@ import qualified Data.Map.Strict
 import qualified Data.Text
 import qualified Data.Text.Lazy
 import qualified Data.Text.Lazy.Builder
+import qualified Text.Builder
+import qualified TextBuilderDev
 
 class Strictness lazy strict where
   toStrict :: lazy -> strict
@@ -23,3 +25,11 @@ class Freezing melted frozen where
 instance Freezing Data.Text.Lazy.Builder.Builder Data.Text.Lazy.Text where
   freeze = Data.Text.Lazy.Builder.toLazyText
   melt = Data.Text.Lazy.Builder.fromLazyText
+
+instance Freezing TextBuilderDev.TextBuilder Data.Text.Text where
+  freeze = TextBuilderDev.buildText
+  melt = TextBuilderDev.text
+
+instance Freezing Text.Builder.Builder Data.Text.Text where
+  freeze = Text.Builder.run
+  melt = Text.Builder.text
