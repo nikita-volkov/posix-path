81f2d69314fbde10d5db88ea76cb11a7d1e3738b
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-26 09:16:54 +0300

    Add atPathAsWorkDir

--- library/Coalmine/FileSystem.hs
+++ library/Coalmine/FileSystem.hs
@@ -1,7 +1,30 @@
-module Coalmine.FileSystem where
+module Coalmine.FileSystem
+  ( atPathAsWorkDir,
+  )
+where
 
 import Coalmine.EvenSimplerPaths (Path)
 import qualified Coalmine.EvenSimplerPaths as Path
 import Coalmine.InternalPrelude hiding (print)
 import Coalmine.Printing
 import qualified Data.Text.IO as TextIO
+import qualified Turtle
+
+atPathAsWorkDir :: Path -> IO a -> IO a
+atPathAsWorkDir path io =
+  bracket acquire release use
+  where
+    acquire = do
+      takeMVar globalCdLock
+      previousWorkDir <- Turtle.pwd
+      Turtle.cd (printCompactAs @String path)
+      return previousWorkDir
+    release previousWorkDir = do
+      Turtle.cd previousWorkDir
+      putMVar globalCdLock ()
+    use _ = io
+
+{-# NOINLINE globalCdLock #-}
+globalCdLock :: MVar ()
+globalCdLock =
+  unsafePerformIO $ newMVar ()
