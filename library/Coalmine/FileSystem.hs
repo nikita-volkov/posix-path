570c5616449bb7b46d461fcca6fa5a0934c488ab
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-26 09:11:23 +0300

    Stub FileSystem

---
+++ library/Coalmine/FileSystem.hs
@@ -0,0 +1,7 @@
+module Coalmine.FileSystem where
+
+import Coalmine.EvenSimplerPaths (Path)
+import qualified Coalmine.EvenSimplerPaths as Path
+import Coalmine.InternalPrelude hiding (print)
+import Coalmine.Printing
+import qualified Data.Text.IO as TextIO
