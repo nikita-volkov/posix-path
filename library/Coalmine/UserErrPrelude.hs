7a5f79f699f29619222ab6a350ab50d0b9957550
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-30 14:40:35 +0300

    Add rethrowUserErrInContext

--- library/Coalmine/UserErrPrelude.hs
+++ library/Coalmine/UserErrPrelude.hs
@@ -2,13 +2,19 @@ module Coalmine.UserErrPrelude
   ( UserErr.UserErr (..),
     UserErr.ToUserErr (..),
     throwUserErr,
+    rethrowUserErrInContext,
   )
 where
 
 import Coalmine.InternalPrelude
 import Coalmine.Name (Name)
+import Coalmine.UserErr (UserErr)
 import qualified Coalmine.UserErr as UserErr
 
-throwUserErr :: MonadError UserErr.UserErr m => Text -> Text -> [Name] -> m a
+throwUserErr :: MonadError UserErr m => Text -> Text -> [Name] -> m a
 throwUserErr reason suggestion contexts =
   throwError $ UserErr.UserErr reason suggestion contexts
+
+rethrowUserErrInContext :: MonadError UserErr m => Name -> m a -> m a
+rethrowUserErrInContext =
+  UserErr.addContextInMonadError
