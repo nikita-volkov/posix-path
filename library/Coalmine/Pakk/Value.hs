3e30917e7181af5b946d9b47b933c6327c75cfeb
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-11-20 20:13:34 +0300

    Update value

--- library/Coalmine/Pakk/Value.hs
+++ library/Coalmine/Pakk/Value.hs
@@ -8,11 +8,30 @@ import Coalmine.PureRandom.SizedSeeded qualified as SizedSeeded
 
 data Value
   = VariantValue Int Text Value
+  | ProductValue [(Text, Value)]
+  | SeqValue (BVec Value)
+
+encode :: Value -> Encoding.Encoding
+encode =
+  error "TODO"
+
+decode :: Schema.Schema -> Decoding.StreamingPtrDecoder Value
+decode =
+  error "TODO"
 
 simulateFromSchema :: Schema.Schema -> SizedSeeded.SizedSeeded Value
 simulateFromSchema = \case
-  Schema.ProductSchema fieldSchemas -> do
-    SizedSeeded.oneOf . fromList . zipWith fieldValue [0 ..] $ fieldSchemas
-  where
-    fieldValue i (name, schema) =
-      VariantValue i name <$> simulateFromSchema schema
+  Schema.ProductSchema fieldSchemas ->
+    fmap ProductValue . traverse fieldValue $ fieldSchemas
+    where
+      fieldValue (name, schema) =
+        (name,) <$> simulateFromSchema schema
+  Schema.SumSchema variantSchemas ->
+    SizedSeeded.oneOf . fromList . zipWith variantValue [0 ..] $ variantSchemas
+    where
+      variantValue i (name, schema) =
+        VariantValue i name <$> simulateFromSchema schema
+  Schema.SeqSchema minLength maxLength elementSchema ->
+    error "TODO"
+  _ ->
+    error "TODO"
