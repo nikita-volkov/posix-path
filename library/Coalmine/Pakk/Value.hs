fdc00add38cbd2e73931f6361f1b36ac39115890
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-11-20 20:01:08 +0300

    Add value randomization

--- library/Coalmine/Pakk/Value.hs
+++ library/Coalmine/Pakk/Value.hs
@@ -4,9 +4,15 @@ import Coalmine.InternalPrelude
 import Coalmine.Pakk.Decoding qualified as Decoding
 import Coalmine.Pakk.Encoding qualified as Encoding
 import Coalmine.Pakk.Schema qualified as Schema
+import Coalmine.PureRandom.SizedSeeded qualified as SizedSeeded
 
 data Value
+  = VariantValue Int Text Value
 
-simulateFromSchema :: Schema.Schema -> Int -> (Value, Int)
-simulateFromSchema =
-  error "TODO"
+simulateFromSchema :: Schema.Schema -> SizedSeeded.SizedSeeded Value
+simulateFromSchema = \case
+  Schema.ProductSchema fieldSchemas -> do
+    SizedSeeded.oneOf . fromList . zipWith fieldValue [0 ..] $ fieldSchemas
+  where
+    fieldValue i (name, schema) =
+      VariantValue i name <$> simulateFromSchema schema
