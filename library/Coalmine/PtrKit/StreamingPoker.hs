f169b58aa249209ba651e855cca9d4a7c27e28a2
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-11-26 18:41:20 +0300

    Add great docs

--- library/Coalmine/PtrKit/StreamingPoker.hs
+++ library/Coalmine/PtrKit/StreamingPoker.hs
@@ -2,9 +2,29 @@ module Coalmine.PtrKit.StreamingPoker where
 
 import Coalmine.InternalPrelude
 
--- |
--- Streaming poking operation.
--- Fills the provided buffer up to a capacity and then gives back control.
+-- | Streaming poking operation.
+--
+-- Fills the provided buffer up to the provided capacity and then gives back control.
+--
+-- Abstracts over the lowest level of operation when serializing data for
+-- interaction with the real-world, so it has the potential to be the most
+-- effecient. In Haskell all real-world data-related operations interact with
+-- pointers, so we do that too. So this abstraction does not even
+-- utilize 'ByteString'.
+--
+-- In fact, since 'ByteString' is itself just an abstraction over a
+-- pointer, 'StreamingPoker' can be used to stream 'ByteString' chunks of a
+-- given size. You can see it as a builder optimized to produce bytestring
+-- chunks with a linear complexity.
+--
+-- You can integrate that API with any streaming library quite easily, since
+-- all of them support lists of bytestrings as the datasource. You can also
+-- integrate with it close to zero cost by directly operating on data buffers.
+-- Or you can do both and integrate with a streaming library more
+-- efficiently.
+--
+-- This abstraction is not the best choice for constructing
+-- a single strict 'ByteString', use 'Coalmine.PtrKit.ImmediatePoker' for that.
 newtype StreamingPoker = StreamingPoker {run :: Ptr Word8 -> Int -> IO WriteIteration}
 
 instance Semigroup StreamingPoker where
