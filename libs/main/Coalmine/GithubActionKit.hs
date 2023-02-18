5d2152e2f6e00bde70e849ebe5a6c7db16698d29
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-02-18 22:50:12 +0300

    Sketch Github Action Kit

---
+++ libs/main/Coalmine/GithubActionKit.hs
@@ -0,0 +1,10 @@
+module Coalmine.GithubActionKit
+  ( HappyPathIO.readOneOf,
+    HappyPathIO.loadRequiredEnv,
+    HappyPathIO.loadNonRequiredEnv,
+    HappyPathIO.findOneFileByExtensions,
+    HappyPathIO.editFile,
+  )
+where
+
+import Coalmine.HappyPathIO qualified as HappyPathIO
