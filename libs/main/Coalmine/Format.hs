92b82523ceb4b375dc6995ba960e8fb8881d4c63
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-03-06 00:57:11 +0300

    Sketch Format

---
+++ libs/main/Coalmine/Format.hs
@@ -0,0 +1,19 @@
+module Coalmine.Format where
+
+import Coalmine.InternalPrelude
+import Data.Attoparsec.Text qualified as Attoparsec
+
+-- |
+-- Value which can be parsed and rendered from a single format.
+-- 
+-- The law is that formatting a value and parsing
+-- should produce the original value.
+class Format a where
+  formatParser :: Attoparsec.Parser a
+  formatAsTextBuilder :: a -> TextBuilder
+
+validateLaws :: (Format a, Eq a) => a -> Bool
+validateLaws val =
+  case Attoparsec.parseOnly (formatParser <* Attoparsec.endOfInput) (to @Text (formatAsTextBuilder val)) of
+    Right parsedVal -> parsedVal == val
+    Left _ -> False
