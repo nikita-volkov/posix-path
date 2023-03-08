b09f615f995cd7e378bcf07e42358119ef5eeb26
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-03-08 19:17:15 +0300

    Group tests

--- tests/tasty/TestSuites/LeanPaths.hs
+++ tests/tasty/TestSuites/LeanPaths.hs
@@ -7,52 +7,54 @@ import Coalmine.Tasty
 import Coalmine.Tasty.TestTrees.Cereal qualified as Cereal
 
 tests =
-  [ Cereal.testEncodeDecode @NormalizedPath Proxy,
-    testProperties "Syntax" $ SyntaxModellingLaws.properties $ Proxy @NormalizedPath,
-    testGroup "Essentials" $
-      [ eqTestCase
-          "Component decomposition works and keeps order"
-          ["src", "main", "java"]
-          (decompose "src/main/java")
-      ],
-    testGroup "Empty" $
-      [ eqTestCase @NormalizedPath
-          "equals empty"
-          mempty
-          "",
-        eqTestCase @NormalizedPath
-          "equals dot"
-          "."
-          ""
-      ],
-    testGroup "Dot" $
-      [ eqTestCase @NormalizedPath
-          "equals empty"
-          mempty
-          ".",
-        eqTestCase @NormalizedPath
-          "acts as a normal component"
-          "src/main"
-          "src/./main"
-      ],
-    testGroup "Trailing slash" $
-      [ eqTestCase @NormalizedPath
-          "Same as without it"
-          "src/main/java"
-          "src/main/java/",
-        eqTestCase
-          "Doesn't produce a trailing component"
-          ["src", "main", "java"]
-          (decompose "src/main/java/")
-      ],
-    testGroup "Multislash" $
-      [ eqTestCase @NormalizedPath
-          "Double"
-          "src/main"
-          "src//main",
-        eqTestCase @NormalizedPath
-          "Triple"
-          "src/main"
-          "src///main"
+  [ testGroup "NormalizedPath" $
+      [ Cereal.testEncodeDecode @NormalizedPath Proxy,
+        testProperties "Syntax" $ SyntaxModellingLaws.properties $ Proxy @NormalizedPath,
+        testGroup "Essentials" $
+          [ eqTestCase
+              "Component decomposition works and keeps order"
+              ["src", "main", "java"]
+              (decompose "src/main/java")
+          ],
+        testGroup "Empty" $
+          [ eqTestCase @NormalizedPath
+              "equals empty"
+              mempty
+              "",
+            eqTestCase @NormalizedPath
+              "equals dot"
+              "."
+              ""
+          ],
+        testGroup "Dot" $
+          [ eqTestCase @NormalizedPath
+              "equals empty"
+              mempty
+              ".",
+            eqTestCase @NormalizedPath
+              "acts as a normal component"
+              "src/main"
+              "src/./main"
+          ],
+        testGroup "Trailing slash" $
+          [ eqTestCase @NormalizedPath
+              "Same as without it"
+              "src/main/java"
+              "src/main/java/",
+            eqTestCase
+              "Doesn't produce a trailing component"
+              ["src", "main", "java"]
+              (decompose "src/main/java/")
+          ],
+        testGroup "Multislash" $
+          [ eqTestCase @NormalizedPath
+              "Double"
+              "src/main"
+              "src//main",
+            eqTestCase @NormalizedPath
+              "Triple"
+              "src/main"
+              "src///main"
+          ]
       ]
   ]
