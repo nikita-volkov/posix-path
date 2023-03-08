4d221c378628453911be8f82b97a5ac2d403b677
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-03-08 18:57:51 +0300

    Migrate some tests from EvenSimplerPaths

--- tests/tasty/TestSuites/LeanPaths.hs
+++ tests/tasty/TestSuites/LeanPaths.hs
@@ -4,9 +4,45 @@ import Coalmine.LeanPaths.NormalizedPath
 import Coalmine.Prelude hiding (Path)
 import Coalmine.SyntaxModellingLaws qualified as SyntaxModellingLaws
 import Coalmine.Tasty
-import Coalmine.Tasty.TestTrees.Cereal
+import Coalmine.Tasty.TestTrees.Cereal qualified as Cereal
 
 tests =
-  [ testEncodeDecode @NormalizedPath Proxy,
-    testProperties "Syntax" $ SyntaxModellingLaws.properties $ Proxy @NormalizedPath
+  [ Cereal.testEncodeDecode @NormalizedPath Proxy,
+    testProperties "Syntax" $ SyntaxModellingLaws.properties $ Proxy @NormalizedPath,
+    testGroup "Empty" $
+      [ eqTestCase @NormalizedPath
+          "equals empty"
+          mempty
+          "",
+        eqTestCase @NormalizedPath
+          "equals dot"
+          "."
+          ""
+      ],
+    testGroup "Dot" $
+      [ eqTestCase @NormalizedPath
+          "equals empty"
+          mempty
+          ".",
+        eqTestCase @NormalizedPath
+          "acts as a normal component"
+          "src/main"
+          "src/./main"
+      ],
+    testGroup "Trailing slash" $
+      [ eqTestCase @NormalizedPath
+          "Same as without it"
+          "src/main/java"
+          "src/main/java/"
+      ],
+    testGroup "Multislash" $
+      [ eqTestCase @NormalizedPath
+          "Double"
+          "src/main"
+          "src//main",
+        eqTestCase @NormalizedPath
+          "Triple"
+          "src/main"
+          "src///main"
+      ]
   ]
