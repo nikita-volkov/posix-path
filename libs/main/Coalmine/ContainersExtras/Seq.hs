a4b8689bee574c99408e661b979d2954738f4dbf
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-03-12 00:06:02 +0300

    Extend Seq utils

--- libs/main/Coalmine/ContainersExtras/Seq.hs
+++ libs/main/Coalmine/ContainersExtras/Seq.hs
@@ -22,3 +22,13 @@ mapInit :: (a -> a) -> Seq a -> Seq a
 mapInit f seq = case viewr seq of
   EmptyR -> mempty
   init :> tailTip -> fmap f init |> tailTip
+
+traverseTailTip :: Applicative f => (a -> f a) -> Seq a -> f (Seq a)
+traverseTailTip f seq = case viewr seq of
+  init :> tailTip -> (init |>) <$> f tailTip
+  EmptyR -> pure mempty
+
+traverseTailTipWithDefault :: Functor f => a -> (a -> f a) -> Seq a -> f (Seq a)
+traverseTailTipWithDefault def f seq = case viewr seq of
+  init :> tailTip -> (init |>) <$> f tailTip
+  EmptyR -> pure <$> f def
