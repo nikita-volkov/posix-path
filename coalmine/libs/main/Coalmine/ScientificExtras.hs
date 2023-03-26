8fe2122639e0abab91acf8a7f09e6950a1752560
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-03-26 15:55:54 +0300

    Add checked scaleToDecimals

--- coalmine/libs/main/Coalmine/ScientificExtras.hs
+++ coalmine/libs/main/Coalmine/ScientificExtras.hs
@@ -3,10 +3,19 @@ module Coalmine.ScientificExtras where
 import Coalmine.InternalPrelude
 import Data.Scientific
 
-scaleToDecimals :: Int -> Scientific -> Integer
-scaleToDecimals decimals a =
+scaleToDecimalsClipping :: Int -> Scientific -> Integer
+scaleToDecimalsClipping decimals a =
   if decimalsDiff >= 0
     then coefficient a * 10 ^ decimalsDiff
     else div (coefficient a) (10 ^ negate decimalsDiff)
+  where
+    decimalsDiff = decimals + base10Exponent a
+
+-- | Fails when the resolution is not enough.
+scaleToDecimals :: Int -> Scientific -> Maybe Integer
+scaleToDecimals decimals a =
+  if decimalsDiff >= 0
+    then Just $ coefficient a * 10 ^ decimalsDiff
+    else Nothing
   where
     decimalsDiff = decimals + base10Exponent a
--- coalmine/tests/tasty/TestSuites/ScientificExtras.hs
+++ coalmine/tests/tasty/TestSuites/ScientificExtras.hs
@@ -7,16 +7,31 @@ import Coalmine.TastyMonadic
 
 declare :: DeclareTestGroup ()
 declare = do
-  "Samples" ?! do
-    assertEqual "" 33 $ ScientificExtras.scaleToDecimals 2 0.33
-    assertEqual "" 330 $ ScientificExtras.scaleToDecimals 3 0.33
-    assertEqual "" 324987 $ ScientificExtras.scaleToDecimals 3 324.987
-    assertEqual "" (-330) $ ScientificExtras.scaleToDecimals 3 (-0.33)
-    assertEqual "" (-324987) $ ScientificExtras.scaleToDecimals 3 (-324.987)
-  "Clips anything down" ?! do
-    assertEqual "" 324987 $ ScientificExtras.scaleToDecimals 3 324.98768
-    assertEqual "" 324987 $ ScientificExtras.scaleToDecimals 3 324.9876
-    assertEqual "" 324987 $ ScientificExtras.scaleToDecimals 3 324.9874
-    assertEqual "" (-324988) $ ScientificExtras.scaleToDecimals 3 (-324.98768)
-    assertEqual "" (-324988) $ ScientificExtras.scaleToDecimals 3 (-324.9876)
-    assertEqual "" (-324988) $ ScientificExtras.scaleToDecimals 3 (-324.9874)
+  "scaleToDecimalsClipping" ?: do
+    "Normal" ?! do
+      assertEqual "" 33 $ ScientificExtras.scaleToDecimalsClipping 2 0.33
+      assertEqual "" 330 $ ScientificExtras.scaleToDecimalsClipping 3 0.33
+      assertEqual "" 324987 $ ScientificExtras.scaleToDecimalsClipping 3 324.987
+      assertEqual "" (-330) $ ScientificExtras.scaleToDecimalsClipping 3 (-0.33)
+      assertEqual "" (-324987) $ ScientificExtras.scaleToDecimalsClipping 3 (-324.987)
+    "Clips anything down" ?! do
+      assertEqual "" 324987 $ ScientificExtras.scaleToDecimalsClipping 3 324.98768
+      assertEqual "" 324987 $ ScientificExtras.scaleToDecimalsClipping 3 324.9876
+      assertEqual "" 324987 $ ScientificExtras.scaleToDecimalsClipping 3 324.9874
+      assertEqual "" (-324988) $ ScientificExtras.scaleToDecimalsClipping 3 (-324.98768)
+      assertEqual "" (-324988) $ ScientificExtras.scaleToDecimalsClipping 3 (-324.9876)
+      assertEqual "" (-324988) $ ScientificExtras.scaleToDecimalsClipping 3 (-324.9874)
+  "scaleToDecimals" ?: do
+    "Normal" ?! do
+      assertEqual "" (Just 33) $ ScientificExtras.scaleToDecimals 2 0.33
+      assertEqual "" (Just 330) $ ScientificExtras.scaleToDecimals 3 0.33
+      assertEqual "" (Just 324987) $ ScientificExtras.scaleToDecimals 3 324.987
+      assertEqual "" (Just (-330)) $ ScientificExtras.scaleToDecimals 3 (-0.33)
+      assertEqual "" (Just (-324987)) $ ScientificExtras.scaleToDecimals 3 (-324.987)
+    "Clips anything down" ?! do
+      assertEqual "" Nothing $ ScientificExtras.scaleToDecimals 3 324.98768
+      assertEqual "" Nothing $ ScientificExtras.scaleToDecimals 3 324.9876
+      assertEqual "" Nothing $ ScientificExtras.scaleToDecimals 3 324.9874
+      assertEqual "" Nothing $ ScientificExtras.scaleToDecimals 3 (-324.98768)
+      assertEqual "" Nothing $ ScientificExtras.scaleToDecimals 3 (-324.9876)
+      assertEqual "" Nothing $ ScientificExtras.scaleToDecimals 3 (-324.9874)
