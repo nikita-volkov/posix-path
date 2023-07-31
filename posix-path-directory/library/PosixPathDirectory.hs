a16003c7a78027341d3c99cd93052efbc4603c9d
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-08-01 00:45:42 +0300

    Fix posix-path-directory

--- posix-path-directory/library/PosixPathDirectory.hs
+++ posix-path-directory/library/PosixPathDirectory.hs
@@ -4,18 +4,19 @@ module PosixPathDirectory
 where
 
 import Coalmine.Prelude hiding (Path)
-import PosixPath
+import PosixPath (Path)
+import PosixPath qualified as Path
 import System.Directory qualified as Directory
 
 -- | Adaptation of "Directory.listDirectory".
 listDirectory :: Path -> IO [Path]
 listDirectory path =
-  Directory.listDirectory (toFilePath path)
+  Directory.listDirectory (Path.toFilePath path)
     >>= traverse parseFilePath
 
 -- * Helpers
 
 parseFilePath :: FilePath -> IO Path
 parseFilePath path =
-  fromFilePath path
+  Path.parseFilePath path
     & maybe (fail ("Invalid path: " <> path)) return
