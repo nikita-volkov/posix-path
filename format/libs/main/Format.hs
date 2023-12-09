b9b5adec13ff1a3e7cbe03cb6092af23b8d1ca9d
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-12-09 14:44:50 +0300

    Sketch Decimal format

--- format/libs/main/Format.hs
+++ format/libs/main/Format.hs
@@ -4,7 +4,16 @@ import Data.Attoparsec.Text qualified as Attoparsec
 import TextBuilderDev
 import Prelude hiding (print)
 
-class Format format where
-  type FormatData format
-  formatPrinter :: format -> FormatData format -> TextBuilder
-  formatParser :: format -> Attoparsec.Parser (FormatData format)
+class Format format value where
+  formatPrinter :: format -> value -> TextBuilder
+  formatParser :: format -> Attoparsec.Parser value
+
+data DecimalFormat = DecimalFormat
+  { scientificNotation :: Bool
+  }
+
+instance Format DecimalFormat Double where
+  formatPrinter =
+    error "TODO"
+  formatParser =
+    error "TODO"
