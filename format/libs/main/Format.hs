70020b65076d452c86ccfe19c7edc78b677d79b3
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-12-09 14:49:23 +0300

    Polish and implement format

--- format/libs/main/Format.hs
+++ format/libs/main/Format.hs
@@ -1,8 +1,24 @@
 module Format where
 
 import Data.Attoparsec.Text qualified as Attoparsec
-import TextBuilderDev
+import Data.Text (Text)
+import Data.Text qualified as Text
+import TextBuilderDev (TextBuilder)
+import TextBuilderDev qualified
 import Prelude hiding (print)
+
+printWithFormat :: (Format f a) => f -> a -> Text
+printWithFormat format value =
+  TextBuilderDev.buildText (formatPrinter format value)
+
+parseWithFormat :: (Format f a) => f -> Text -> Either Text a
+parseWithFormat format input =
+  case Attoparsec.parseOnly parser input of
+    Right value -> Right value
+    Left errorString -> Left (Text.pack errorString)
+  where
+    parser =
+      formatParser format <* Attoparsec.endOfInput
 
 class Format format value where
   formatPrinter :: format -> value -> TextBuilder
