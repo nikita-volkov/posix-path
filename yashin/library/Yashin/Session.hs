fb8b316a1bdf8e4515e68b03ccc705d58fcef2cb
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-01-28 23:50:47 +0300

    Add run for Session

--- yashin/library/Yashin/Session.hs
+++ yashin/library/Yashin/Session.hs
@@ -2,7 +2,11 @@ module Yashin.Session where
 
 import Amazonka qualified as Azk
 import Coalmine.Prelude hiding (String)
-import Conduit (ResourceT)
+import Control.Monad.Trans.Resource (ResourceT, runResourceT)
+
+run :: Session a -> Azk.Env -> IO (Either Azk.Error a)
+run (Session run) =
+  runResourceT . run
 
 newtype Session a
   = Session (Azk.Env -> ResourceT IO (Either Azk.Error a))
