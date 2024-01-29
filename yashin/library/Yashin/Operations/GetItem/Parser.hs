5bcafc9eaf955a5824e1f1296ef984e64103e433
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-01-29 03:30:08 +0300

    Implement GetItem

--- yashin/library/Yashin/Operations/GetItem/Parser.hs
+++ yashin/library/Yashin/Operations/GetItem/Parser.hs
@@ -9,12 +9,32 @@ newtype Response a = Response
   { run :: Azk.GetItemResponse -> Either [Error] a
   }
   deriving
-    (Functor, Applicative, Alternative, Monad, MonadPlus)
+    (Functor, Applicative, Alternative)
     via (ReaderT Azk.GetItemResponse (Except [Error]))
 
 data Error
   = AttributesError [Attributes.AttributesError]
   | AttributesNotFoundError
+  | UnexpectedStatusError UnexpectedStatus
+
+data UnexpectedStatus = UnexpectedStatus
+  { actual :: Int,
+    expected :: Int
+  }
+
+okStatus :: Response ()
+okStatus =
+  Response \response ->
+    if response.httpStatus < 400
+      then Right ()
+      else Left [UnexpectedStatusError UnexpectedStatus {actual = response.httpStatus, expected = 200}]
+
+resourceNotFoundStatus :: Response ()
+resourceNotFoundStatus =
+  Response \response ->
+    if response.httpStatus >= 400 && response.httpStatus < 500
+      then Right ()
+      else Left [UnexpectedStatusError UnexpectedStatus {actual = response.httpStatus, expected = 400}]
 
 attributes :: Attributes.Attributes a -> Response a
 attributes parser =
