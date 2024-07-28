31191602484b5074ff8815dcec825d5b64b9fd31
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-07-28 12:56:01 +0300

    Add more instances

--- partial-isomorphism-class/library/PartialIsomorphismClass.hs
+++ partial-isomorphism-class/library/PartialIsomorphismClass.hs
@@ -1,7 +1,10 @@
 module PartialIsomorphismClass where
 
 import Coalmine.Prelude
+import Data.Aeson qualified as Aeson
+import Data.ByteString.Lazy qualified as ByteString.Lazy
 import Data.Text.Encoding qualified as TextEncoding
+import Data.Text.Lazy qualified as Text.Lazy
 
 class GeneralizationOf special general where
   generalizeFrom :: special -> general
@@ -30,3 +33,15 @@ instance GeneralizationOf Natural Integer where
 instance GeneralizationOf Text ByteString where
   generalizeFrom = TextEncoding.encodeUtf8
   specializeTo = either (const Nothing) Just . TextEncoding.decodeUtf8'
+
+instance GeneralizationOf Aeson.Value Text where
+  generalizeFrom = Text.Lazy.toStrict . Aeson.encodeText
+  specializeTo = either (const Nothing) Just . Aeson.eitherDecodeStrictText
+
+instance GeneralizationOf Aeson.Value LazyByteString where
+  generalizeFrom = Aeson.encode
+  specializeTo = either (const Nothing) Just . Aeson.decode
+
+instance GeneralizationOf Aeson.Value ByteString where
+  generalizeFrom = ByteString.Lazy.toStrict . Aeson.encode
+  specializeTo = either (const Nothing) Just . Aeson.decodeStrict
