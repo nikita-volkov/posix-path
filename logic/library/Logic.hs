63e4e33ac09e78ef3fd431c65c9fee15e91510f4
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-10-10 19:42:48 +0300

    Add lifts

--- logic/library/Logic.hs
+++ logic/library/Logic.hs
@@ -2,31 +2,43 @@ module Logic where
 
 import Coalmine.Prelude
 
-newtype Logic ctx res
-  = Logic (forall m. (Monad m) => ctx m -> m res)
+newtype Logic ops res
+  = Logic (forall m. (Monad m) => ops m -> m res)
 
-instance Functor (Logic ctx) where
+instance Functor (Logic ops) where
   fmap f (Logic run) =
     Logic (liftM f . run)
 
-instance Applicative (Logic ctx) where
+instance Applicative (Logic ops) where
   pure a =
     Logic (\_ -> pure a)
   Logic runL <*> Logic runR =
-    Logic (\ctx -> runL ctx <*> runR ctx)
+    Logic (\ops -> runL ops <*> runR ops)
 
-instance Monad (Logic ctx) where
+instance Monad (Logic ops) where
   return = pure
   Logic runL >>= k =
     Logic
-      ( \ctx -> do
-          param <- runL ctx
+      ( \ops -> do
+          param <- runL ops
           case k param of
-            Logic runR -> runR ctx
+            Logic runR -> runR ops
       )
 
-run :: (Monad m) => Logic ctx res -> ctx m -> m res
+run :: (Monad m) => Logic ops res -> ops m -> m res
 run (Logic run) = run
 
-runOnIdentity :: Logic ctx res -> ctx Identity -> res
+runOnIdentity :: Logic ops res -> ops Identity -> res
 runOnIdentity (Logic run) = runIdentity . run
+
+liftOp1 :: (forall m. ops m -> a -> m res) -> a -> Logic ops res
+liftOp1 lifter a =
+  Logic \ops -> lifter ops a
+
+liftOp2 :: (forall m. ops m -> a -> b -> m res) -> a -> b -> Logic ops res
+liftOp2 lifter a b =
+  Logic \ops -> lifter ops a b
+
+liftOp3 :: (forall m. ops m -> a -> b -> c -> m res) -> a -> b -> c -> Logic ops res
+liftOp3 lifter a b c =
+  Logic \ops -> lifter ops a b c
