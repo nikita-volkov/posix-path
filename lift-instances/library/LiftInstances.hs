4360d0a35824dc7bce58918ec9a74a76c2de8fae
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-01-30 06:49:37 +0300

    Modernize lift-instances

--- lift-instances/library/LiftInstances.hs
+++ lift-instances/library/LiftInstances.hs
@@ -1,20 +1,6 @@
-{-# OPTIONS_GHC -Wno-orphans #-}
 {-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module LiftInstances where
 
-import Language.Haskell.TH.Lift
-import Prelude
-
-#if __GLASGOW_HASKELL__ < 908
-deriving instance Lift (Fixed a)
-#endif
-
-deriveLiftMany
-  [ ''Day,
-    ''DiffTime,
-    ''LocalTime,
-    ''TimeOfDay,
-    ''TimeZone,
-    ''UTCTime
-  ]
+import LiftInstances.Fixed ()
---
+++ lift-instances/library/LiftInstances/Fixed.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -Wno-orphans  -Wno-unused-imports #-}
+
+module LiftInstances.Fixed where
+
+import Data.Fixed
+import Language.Haskell.TH.Lift
+
+#if __GLASGOW_HASKELL__ < 908
+deriving instance Lift (Fixed a)
+#endif
